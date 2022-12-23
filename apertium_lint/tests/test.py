97f5964920dabe0f90d77e998439a7bf4f849c5a
Daniel Swanson <awesomeevildudes@gmail.com>
2022-12-23 10:45:47 -0500

    rtx operators should never have been this varied

--- apertium_lint/tests/test.py
+++ apertium_lint/tests/test.py
@@ -73,3 +73,17 @@ class ModesInstallDeps(LintTest):
     expected_checks = [
         (6, 'install-deps')
     ]
+
+class RTXInconsistentOperators(LintTest):
+    file_name = 'test.rtx'
+    file_contents = '''
+number = sg pl;
+NP: _ ;
+num: _ ;
+NP -> num [$number=(if (1.lem equal-cl one) sg else pl)]
+      { 1[number=(if ($lem equal_CASE_LESS one) sg else pl)] } ;
+'''
+    expected_class = 'RTXLinter'
+    expected_checks = [
+        (6, 'inconsistent-operator')
+    ]
